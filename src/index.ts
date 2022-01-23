a4e1c46d2d48379b938c3998affa09cd4e419492
Dalmo Mendes <dalmosilvamendes@gmail.com>
2022-01-23 19:04:12 -0300

    update - automatizar execução!

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Executei!!!");
+import express, {Request, Response, NextFunction} from 'express';
+
+const app = express();
+
+// rotas e resolves:
+app.get('/status', (req: Request, res: Response, next: NextFunction)=> {
+    res.status(200).send({ foo: 'bar'});
+});
+
+// Subir servidor na porta 3000
+app.listen(3000, () => {
+    console.log('Servidor tá On na porta 3000!');
+})
